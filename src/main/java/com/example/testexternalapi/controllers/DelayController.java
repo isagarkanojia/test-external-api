10e16a216c5c1bd9140b959e1bf349c40bb19e58
Sagar Kanojia <isagarkanojia@gmail.com>
2022-09-15 14:56:11 +0530

    Add Get client IP  API

--- src/main/java/com/example/testexternalapi/controllers/DelayController.java
+++ src/main/java/com/example/testexternalapi/controllers/DelayController.java
@@ -3,7 +3,12 @@ package com.example.testexternalapi.controllers;
 import com.example.testexternalapi.dtos.CDto;
 import com.example.testexternalapi.dtos.Request;
 import com.google.gson.Gson;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 @RestController
 @RequestMapping("delay")
@@ -63,6 +68,43 @@ public class DelayController {
         dto.getResult().setPan(request.getPan());
 
         return dto;
+    }
+
+
+    private final String LOCALHOST_IPV4 = "127.0.0.1";
+    private final String LOCALHOST_IPV6 = "0:0:0:0:0:0:0:1";
+
+    @GetMapping("client-ip")
+    public String clientIP(HttpServletRequest request) {
+        String ipAddress = request.getHeader("X-Forwarded-For");
+        if(StringUtils.isEmpty(ipAddress) || "unknown".equalsIgnoreCase(ipAddress)) {
+            ipAddress = request.getHeader("Proxy-Client-IP");
+        }
+
+        if(StringUtils.isEmpty(ipAddress) || "unknown".equalsIgnoreCase(ipAddress)) {
+            ipAddress = request.getHeader("WL-Proxy-Client-IP");
+        }
+
+        if(StringUtils.isEmpty(ipAddress) || "unknown".equalsIgnoreCase(ipAddress)) {
+            ipAddress = request.getRemoteAddr();
+            if(LOCALHOST_IPV4.equals(ipAddress) || LOCALHOST_IPV6.equals(ipAddress)) {
+                try {
+                    InetAddress inetAddress = InetAddress.getLocalHost();
+                    ipAddress = inetAddress.getHostAddress();
+                } catch (UnknownHostException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        if(!StringUtils.isEmpty(ipAddress)
+                && ipAddress.length() > 15
+                && ipAddress.indexOf(",") > 0) {
+            ipAddress = ipAddress.substring(0, ipAddress.indexOf(","));
+        }
+
+        return ipAddress;
+
     }
 
 }
